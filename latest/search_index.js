b7a597653738201ea38e852a0b8907ff99246f2f
Documenter.jl <documenter@juliadocs.github.io>
2022-11-27 01:21:08 +0000

    build based on 75b5d16

---
+++ latest/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Duo","category":"page"},{"location":"#Duo","page":"Home","title":"Duo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Duo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Duo]","category":"page"}]
+}
